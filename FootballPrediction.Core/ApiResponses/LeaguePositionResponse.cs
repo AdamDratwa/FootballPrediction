4e18eb5713d37ea8f3de62b34eae9ef93d5804a2
adamdratwa <adam.dratwa@fabres.pl>
2017-03-16 12:49:12 +0100

    Adding league positions

---
+++ FootballPrediction.Core/ApiResponses/LeaguePositionResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace FootballPrediction.Core.ApiResponses
+{
+    public class LeaguePositionResponse : ApiElement
+    {
+        public int Position { get; set; }
+        public string TeamName { get; set; }
+        public int PlayedGames { get; set; }
+        public int Points { get; set; }
+        public int Goals { get; set; }
+        public int GoalsAgainst { get; set; }
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Loses { get; set; }
+        public Stats Home { get; set; }
+        public Stats Away { get; set; }
+    }
+}
---
+++ FootballPrediction.Core/ApiResponses/LeagueTableResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FootballPrediction.Core.ApiResponses
+{
+    public class LeagueTableResponse : ApiElement
+    {
+        public IEnumerable<LeaguePositionResponse> Standing { get; set; }
+    }
+}
